9ee09352563f8295a65121a34f0906b6e9131bfd
iyana-wilson <77690268+iyana-wilson@users.noreply.github.com>
2021-12-12 15:27:08 -0500

    Changed link to https

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -40,7 +40,7 @@ if(loaded){
     let apiKey= "361e0aa61c613a70c483b8ca214c4ec2";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
-    let apiUrl = `http://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
     
     axios.get(apiUrl).then(handleRequest);
 
